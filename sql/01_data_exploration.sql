223d12ee74753a7643146e4ff448e0d05d51d022
Hazim-sulaiman <hazimof@live.com>
2026-06-22 10:42:53 +0300

    Add database overview section

--- sql/01_data_exploration.sql
+++ sql/01_data_exploration.sql
@@ -5,7 +5,22 @@
 -- ==========================================
 -- DATABASE OVERVIEW
 -- ==========================================
+-- Show all tables in the database
+SHOW TABLES;
+-- ==========================================
+-- TABLE STRUCTURE EXPLORATION
+-- ==========================================
+-- customer table structure
+DESCRIBE customers;
 -- Total Customers
+SELECT COUNT(*) AS total_customers
+FROM customers;
 -- Total Orders
+SELECT COUNT(*) AS total_orders
+FROM orders;
 -- Total Products
+SELECT COUNT(*) AS total_products
+FROM products;
 -- Total Payments
+SELECT COUNT(*) AS total_payments
+FROM payments;
